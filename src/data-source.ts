458bf8228cb6d1967b5a974626e99eb8b843906b
TigranJaghatspanyan <tigranjaghatspanyan@gmail.com>
2024-08-29 19:57:45 +0400

    minor change

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,13 @@
-import { DataSource } from 'typeorm';
-import { User } from './entity/User';
-import { Organization } from './entity/Organization';
-import { Item } from './entity/Items';
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { Organization } from "./entity/Organization";
+import { Item } from "./entity/Items";
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   url: process.env.DATABASE_URL,
   entities: [User, Organization, Item],
   synchronize: true,
   logging: true,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: false,
 });
